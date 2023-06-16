0f0eaf366c43ecdd2938e69d9afd87f06c1466c7
Isaacse <125591950+Isaacse@users.noreply.github.com>
2023-06-16 09:32:26 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <?php  
+     $edad= 25;
+          switch ($edad) 
+          {
+             case 20:
+             echo "Tiene casi de 25 años";break;
+             case 25:
+             echo "tiene 25 años";break;
+             default:
+             echo "no tiene 20 ni 25 años";break;
+          }
+             
+    ?> 
+    <script src="https://replit.com/public/js/replit-badge.js" theme="blue" defer></script> 
+  </body>
+</html>
